4c9e869a52053396362a49cdf4535c7b94199d99
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2024-07-14 12:51:26 +0100

    Borked card grid implementation

--- src/fh_franken_ui/card.py
+++ src/fh_franken_ui/card.py
@@ -1,11 +1,26 @@
+from typing import Sequence, Any
+
 from fasthtml import xt
 
-__all__ = ["Card", ]
+__all__ = [
+    "Card",
+]
 
 
-def Card(*content, title: str, paragraph: str) -> str:
+def Card(
+    title: Sequence[Any] | str | None = None,
+    body: Sequence[Any] | None = None,
+    footer: Sequence[Any] | str | None = None,
+    w_numerator: int = 1,
+    w_denominator: int = 4,
+) -> list:
+    if isinstance(title, str): 
+        title = [title]
+    if isinstance(footer, str): 
+        footer = [footer]        
     return xt.Div(
-        xt.H3(title, cls="uk-card-title"),
-        xt.P(paragraph, cls="uk-margin"),
-        cls="uk-width-1-2@m uk-card uk-card-body uk-card-default",
+        xt.Div(xt.H3(*title, cls="uk-card-title"), cls="uk-card-header") if title else xt.Span(),
+        xt.Div(*body, cls="uk-card-body") if body else xt.Span(),
+        xt.Div(*footer, cls="uk-card-footer") if footer else xt.Span(),
+        cls=f"uk-card uk-card-body uk-card-default uk-width-{w_numerator}-{w_denominator}@m"
     )
