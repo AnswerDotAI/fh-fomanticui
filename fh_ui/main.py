715b48683b10727b348673aec35b118e803a1e73
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-07-15 13:53:28 +0100

    Setup to dynamically change stylesheet with htmx

--- fh_ui/main.py
+++ fh_ui/main.py
@@ -2,13 +2,27 @@ from fasthtml.common import *
 from fh_ui.card import Card
 import uvicorn
 
+# Since this is just a CSS POC, for now we don't include jQuery or the js for each CSS framework
+stylesheets = {
+    'pico': 'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css',
+    'bootstrap': 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+    # TODO: add daisyui. It also requires tailwindcss separately from https://cdn.tailwindcss.com
+    # 'tailwind_daisy': 'https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css',
+    'frankenui': 'https://unpkg.com/franken-wc@0.0.2/dist/css/slate.min.css',
+    'semanticui': 'https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.css',
+}
+
 hdrs = [
-    Meta(name='description', content='FastHTML Semantic/Fomantic UI'),
-    # For Fomantic UI, since this is just a CSS POC, for now we don't include jQuery or the js
-    Link(href='https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.css', rel='stylesheet'),
+    Meta(name='description', content='fh-ui: FastHTML UI'),
+    Link(href=stylesheets['semanticui'], rel='stylesheet', id='dynamic-stylesheet'),
+    Script(src='https://unpkg.com/htmx.org@2.0.1/dist/htmx.js'),
 ]
 
 app, rt = fast_app(hdrs=hdrs, live=True, default_hdrs=False)
+
+@rt('/change_stylesheet')
+def change_stylesheet(style: str):
+    return Link(href=stylesheets[style], rel='stylesheet', id='dynamic-stylesheet')
 
 @rt("/")
 def get():
@@ -26,10 +40,14 @@ def get():
             Select(
                 Option('Pico CSS', value='pico'),
                 Option('Bootstrap', value='bootstrap'),
-                Option('Tailwind CSS with DaisyUI', value='tailwind_daisy'),
+                # Option('Tailwind CSS with DaisyUI', value='tailwind_daisy'),
                 Option('Franken UI', value='frankenui'),
                 Option('Semantic UI / Fomantic UI', value='semanticui'),
-                cls='ui dropdown'
+                cls='ui dropdown',
+                hx_post='/change_stylesheet',
+                hx_trigger='change',
+                hx_target='#dynamic-stylesheet',
+                hx_swap='outerHTML'
             ),
             
             H2('Cards', cls='ui header'),
